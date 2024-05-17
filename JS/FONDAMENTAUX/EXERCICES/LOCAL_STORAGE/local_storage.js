5cca6f60bf46192f714c2240d0d67356d781fd5f
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-05-17 16:47:49 +0200

    Updated 2 files

--- JS/FONDAMENTAUX/EXERCICES/LOCAL_STORAGE/local_storage.js
+++ JS/FONDAMENTAUX/EXERCICES/LOCAL_STORAGE/local_storage.js
@@ -1,1 +1,24 @@
 window.localStorage.setItem('user', 'Jason');
+window.localStorage.setItem('user2', 'Anais');
+
+// récupérer une clé
+const jason = window.localStorage.getItem('user');
+console.log(jason);
+
+// Supprimer une clé
+localStorage.removeItem('user2');
+
+// Purger le localStorage
+localStorage.clear;
+
+// Création d'un objet customer1
+const customer = {
+    firstname: "Joséphine",
+    age: 27
+}
+
+// Transformer l'objet en chaine
+const customerStr = JSON.stringify(customerObj);
+
+// On place cet objet "chainifié" dans le localStorage
+localStorage.setItem('customer1', customerStr);
